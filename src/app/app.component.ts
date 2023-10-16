943202b0f6b948182cca326e3352610222272424
Le Tinh <ltinh2908@gmail.com>
2023-10-16 08:36:13 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycalculator';
+
+  toshow = ''
+
+  currvalue = ''
+
+  writetoinput(value:string){
+    this.currvalue = this.currvalue + value
+    this.toshow = this.currvalue
+  }
+  equals(){
+    this.toshow = eval(this.currvalue)
+    this.currvalue = this.toshow
+  }
+
+  clear(){
+    this.currvalue=''
+    this.toshow = this.currvalue
+  }
+
+  back(){
+    this.currvalue = this.currvalue.slice(0,-1)
+    this.toshow = this.currvalue
+    if(this.toshow == ''){this.toshow = ''}
+  }
+
+  calcvalue(solve: any){
+    if(solve.chatAt(0) == '0'){
+      solve = solve.slice(1,)
+    }
+    this.toshow = eval(solve)
+  }
 }
